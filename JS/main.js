726a7bc93dd9c3db81f3663e974d5d26d1d49b7b
Dudarev <dima_dudariev@mail.ru>
2020-03-14 16:19:32 +0300

    Добавил 2-е модальное окно

--- JS/main.js
+++ JS/main.js
@@ -5,16 +5,16 @@ window.addEventListener('DOMContentLoaded', () => {
     const popupOne = () => {
         const callBtn = document.querySelectorAll('a.call-btn');
         const popupCall = document.querySelector('.popup-call');
-        const captureContent = document.querySelector('.popup-content');
+        const captureContent = document.querySelectorAll('.popup-content');
 
         const addPopup = () => {
             let count = 100;
             popupCall.style.display = 'block';
-            captureContent.style.top = '100%';
+            captureContent[0].style.top = '100%';
 
             let id = setInterval(()=>{
                 count--;
-                captureContent.style.top = count+"%";
+                captureContent[0].style.top = count+"%";
                 if(count === 20){
                     clearInterval(id);
                 }
@@ -42,10 +42,27 @@ window.addEventListener('DOMContentLoaded', () => {
     };
     popupOne();
 
-    //Кнопка больше
+    //Кнопка больше + модалка 2
     const addsentence = () => {
         const stocks = document.querySelector('#stocks');
         const product = document.querySelectorAll('.col-xs-12.col-sm-6.col-md-4');
+
+        const popupDiscount = document.querySelector('.popup-discount');
+        const discountContent = document.querySelectorAll('.popup-content');
+
+        const addPopupTwo = () => {
+            let count = 100;
+            popupDiscount.style.display = 'block';
+            discountContent[1].style.top = '100%';
+
+            let id = setInterval(()=>{
+                count--;
+                discountContent[1].style.top = count+"%";
+                if(count === 20){
+                    clearInterval(id);
+                }
+            }, 5);
+        };
 
         stocks.addEventListener('click', (event) => {
             let target = event.target;
@@ -55,9 +72,36 @@ window.addEventListener('DOMContentLoaded', () => {
                     item.classList.remove('hidden');
                     item.classList.remove('visible-sm-block');
                 });
+            } else if(target.classList[0] === 'discount-btn'){
+                addPopupTwo();
+            }
+        });
+
+        popupDiscount.addEventListener('click', (event) => {
+            event.preventDefault();
+            let target = event.target;
+
+            if(target.className === 'popup-close'){
+                popupDiscount.style.display = 'none';
+            }else {
+                target = target.closest('.popup-content');
+
+                if(!target){
+                    popupDiscount.style.display = 'none';
+                }
+
             }
         });
     };
 
     addsentence();
+
+    //Скидка
+
+    const  discount = () => {
+        
+    };
+
+    discount();
+
 });
